111106961b9b6fa1fef66badabf9e0b512bdc2a4
maro <marek.borecki88@gmail.com>
2022-05-26 21:10:14 +0300

    add log

--- src/main/java/com/example/reactiveprogrammingtutorial/service/FluxAndMonoServices.java
+++ src/main/java/com/example/reactiveprogrammingtutorial/service/FluxAndMonoServices.java
@@ -1,19 +1,26 @@
 package com.example.reactiveprogrammingtutorial.service;
 
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 import java.util.List;
 
 public class FluxAndMonoServices {
     public Flux<String> fruitsFlux() {
-        return Flux.fromIterable(List.of("Mango", "Orange", "Banana"));
+        return Flux.fromIterable(List.of("Mango", "Orange", "Banana")).log();
     }
-    public Mono<>
+    public Mono<String> fruitsMono() {
+        return Mono.just("Mango").log();
+    }
     public static void main(String... args) {
         FluxAndMonoServices fluxAndMonoServices = new FluxAndMonoServices();
         fluxAndMonoServices.fruitsFlux()
                 .subscribe(s -> {
                     System.out.printf("s = %s\n", s);
+                });
+        fluxAndMonoServices.fruitsMono()
+                .subscribe(s -> {
+                    System.out.printf("Mono -> s = %s\n", s);
                 });
     }
 }
